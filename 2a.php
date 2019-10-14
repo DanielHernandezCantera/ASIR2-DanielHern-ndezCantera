6339a5038fa450731ca820484062b4a0dec6dbde
DanielHernandezCantera <danielhernandezcantera@gmail.com>
2019-10-14 15:24:54 +0200

    variables

---
+++ 2a.php
@@ -0,0 +1,23 @@
+<h1>Variables</h1>
+
+<?php
+//suma de 2 numeros en php
+
+$a=800;
+$b=-70;
+
+
+$mas=$a+$b;
+
+$menos=$a-$b;
+
+$por=$a*$b;
+
+$entre=$a/$b;
+
+echo "numeros: 800 y -70 <br>";
+echo "suma.$mas. <br>";
+echo "resta.$menos. <br>";
+echo "producto.$por. <br>";
+echo "cociente.$entre. <br>";
+ ?>
